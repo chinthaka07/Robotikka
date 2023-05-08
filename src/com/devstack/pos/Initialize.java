aaf0adbb61fbda41197cf1efe88eaa0be3920982
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-08 22:47:48 +0530

    created signup form

--- src/com/devstack/pos/Initialize.java
+++ src/com/devstack/pos/Initialize.java
@@ -1,4 +1,21 @@
 package com.devstack.pos;
 
-public class Initialize {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Initialize extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
+        primaryStage.setTitle("Login");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+    }
 }
