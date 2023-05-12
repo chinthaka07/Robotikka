f31a292dff9c1960a93416205b2d9ec693b52278
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-12 21:38:36 +0530

    created customer package

--- src/com/devstack/pos/dao/custom/ProductDao.java
+++ src/com/devstack/pos/dao/custom/ProductDao.java
@@ -1,4 +1,14 @@
 package com.devstack.pos.dao.custom;
 
+import com.devstack.pos.dto.Product;
+import com.devstack.pos.entity.User;
+
+import java.util.List;
+
 public interface ProductDao {
+    public boolean saveProduct(Product product);
+    public boolean updateProduct(Product product);
+    public boolean deleteProduct(int code);
+    public Product findProduct(int code);
+    public List<Product> findAllProducts();
 }
