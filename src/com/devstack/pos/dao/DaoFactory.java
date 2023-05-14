db9998e0f442c7481de0a2906b57a48c16c552fe
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-14 09:34:24 +0530

    finalized dao impl 1

--- src/com/devstack/pos/dao/DaoFactory.java
+++ src/com/devstack/pos/dao/DaoFactory.java
@@ -7,23 +7,26 @@ import com.devstack.pos.enums.DaoType;
 
 public class DaoFactory {
     private static DaoFactory daoFactory;
-    private DaoFactory(){}
 
-    public static DaoFactory getInstance(){
-        return (daoFactory==null)?daoFactory = new DaoFactory():daoFactory;
+    private DaoFactory() {
     }
 
-    public SuperDao getDao(DaoType daoType){
-        switch (daoType){
+    public static DaoFactory getInstance() {
+        return (daoFactory == null) ? daoFactory = new DaoFactory() : daoFactory;
+    }
+
+    public <T> T getDao(DaoType daoType) {
+        switch (daoType) {
             case USER:
-                return new UserDaoImpl();
+                return (T) new UserDaoImpl();
             case CUSTOMER:
-                return new CustomerDaoImpl();
+                return (T) new CustomerDaoImpl();
             case PRODUCT:
-                return new ProductDaoImpl();
+                return (T) new ProductDaoImpl();
             default:
                 return null;
-
         }
     }
+
+
 }
